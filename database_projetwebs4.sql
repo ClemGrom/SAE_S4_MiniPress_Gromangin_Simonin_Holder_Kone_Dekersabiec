feb6d7de0aefb2d88b6e364f972b641549398d40
Nzosim <nzosim54@gmail.com>
2023-06-15 10:40:35 +0200

    data dbb

--- database_projetwebs4.sql
+++ database_projetwebs4.sql
@@ -51,11 +51,6 @@ CREATE TABLE `CATEGORIE` (
 -- Déchargement des données de la table `CATEGORIE`
 --
 
-INSERT INTO `CATEGORIE` (`id`, `titre`, `description`) VALUES
-(1, 'Actualité', "Informations complètes et à jour, compilées à partir de sources d'actualités du monde entier."),
-(2, 'Evenements', 'Informations sur les evenements dans le grand est.'),
-(3, 'Prévention', 'Attention, notre prévention vous vient en aide');
-
 -- --------------------------------------------------------
 
 --
---
+++ sql/miniPress.data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `CATEGORIE` (`id`, `titre`, `description`) VALUES
+(1, 'Actualité', "Informations complètes et à jour, compilées à partir de sources d'actualités du monde entier."),
+(2, 'Evenements', 'Informations sur les evenements dans le grand est.'),
+(3, 'Prévention', 'Attention, notre prévention vous vient en aide');
