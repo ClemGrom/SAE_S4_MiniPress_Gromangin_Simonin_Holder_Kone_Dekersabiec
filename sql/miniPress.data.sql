955ac8deaccdcc329956ae95f6cf2728f68b830e
Jules <jules.holder.54@gmail.com>
2023-06-15 14:35:38 +0200

    BDD ajout categories pour les articles

--- sql/miniPress.data.sql
+++ sql/miniPress.data.sql
@@ -1,5 +1,6 @@
-INSERT INTO `ARTICLE`(`id`, `titre`, `resume`, `contenu`, `date_crea`) VALUES
-(1, 'Nouvelles découvertes en astronomie', 'Des astronomes ont récemment fait une découverte majeure dans le domaine de l\'astronomie.', "Depuis des siècles, les astronomes scrutent le ciel à la recherche de nouvelles planètes et de nouveaux mondes. Récemment, une équipe d'astronomes a fait une avancée significative en découvrant une planète unique dans notre vaste univers : la planète Jules.
+INSERT INTO `ARTICLE`(`id`, `titre`, `resume`, `contenu`, `date_crea`, `categ_id`)
+VALUES (1, 'Nouvelles découvertes en astronomie',
+        'Des astronomes ont récemment fait une découverte majeure dans le domaine de l\'astronomie.', "Depuis des siècles, les astronomes scrutent le ciel à la recherche de nouvelles planètes et de nouveaux mondes. Récemment, une équipe d'astronomes a fait une avancée significative en découvrant une planète unique dans notre vaste univers : la planète Jules.
 
 Jules, nommée en l'honneur du célèbre astronome Jules Verne, se trouve à une distance impressionnante de notre système solaire, à des millions d'années-lumière de la Terre. Cette planète fascinante présente des caractéristiques uniques qui la distinguent des autres planètes connues jusqu'à présent.
 
@@ -9,8 +10,9 @@ Les données recueillies jusqu'à présent indiquent que Jules orbite autour d'u
 
 Les astronomes espèrent que l'étude approfondie de la planète Jules fournira des informations précieuses sur la formation des planètes, l'évolution des systèmes stellaires et peut-être même sur la possibilité de vie extraterrestre. Les recherches futures comprendront des observations détaillées à l'aide de télescopes spatiaux avancés et de missions d'exploration robotiques.
 
-En conclusion, la découverte de la planète Jules constitue une avancée majeure dans le domaine de l'astronomie. Cette planète mystérieuse offre de nouvelles perspectives passionnantes sur l'univers et suscite de nombreuses questions sur la possibilité d'une vie extraterrestre. Les astronomes continueront à étudier Jules de près, dans l'espoir de percer les secrets de cet autre monde situé au-delà de notre portée actuelle.", '2023-06-14'),
-(2, 'Les dernières tendances de la mode', 'Découvrez les dernières tendances de la mode pour cette saison.', "La mode est en constante évolution, et chaque saison apporte son lot de nouvelles tendances. Cette fois-ci, c'est le costume jogging tong qui fait sensation. Cette combinaison improbable semble opposer deux mondes différents, mais crée en réalité une harmonie surprenante.
+En conclusion, la découverte de la planète Jules constitue une avancée majeure dans le domaine de l'astronomie. Cette planète mystérieuse offre de nouvelles perspectives passionnantes sur l'univers et suscite de nombreuses questions sur la possibilité d'une vie extraterrestre. Les astronomes continueront à étudier Jules de près, dans l'espoir de percer les secrets de cet autre monde situé au-delà de notre portée actuelle.",
+        '2023-06-14', 1),
+       (2, 'Les dernières tendances de la mode', 'Découvrez les dernières tendances de la mode pour cette saison.', "La mode est en constante évolution, et chaque saison apporte son lot de nouvelles tendances. Cette fois-ci, c'est le costume jogging tong qui fait sensation. Cette combinaison improbable semble opposer deux mondes différents, mais crée en réalité une harmonie surprenante.
 
 Le costume jogging tong est la réponse parfaite à notre quête incessante d'un confort ultime sans sacrifier le style. Imaginez-vous dans un ensemble de jogging luxueux, avec sa coupe décontractée et ses matières douces qui épousent votre corps. Ajoutez à cela une paire de tongs sophistiquées, aux détails raffinés et à la silhouette élégante.
 
@@ -22,9 +24,11 @@ Si vous souhaitez adopter une approche plus audacieuse, osez le costume jogging 
 
 Il est important de noter que cette tendance du costume jogging tong est avant tout une expression de la liberté individuelle en matière de style. Elle encourage chacun à exprimer sa créativité et à repousser les conventions établies.
 
-En conclusion, le costume jogging tong est une tendance surprenante qui allie le confort du sportswear avec une touche d'élégance. Osez cette combinaison inattendue et créez votre propre style en jouant avec les contrastes. La mode est une forme d'expression personnelle, alors n'hésitez pas à explorer de nouvelles tendances et à vous amuser avec votre garde-robe.", '2023-06-13'),
+En conclusion, le costume jogging tong est une tendance surprenante qui allie le confort du sportswear avec une touche d'élégance. Osez cette combinaison inattendue et créez votre propre style en jouant avec les contrastes. La mode est une forme d'expression personnelle, alors n'hésitez pas à explorer de nouvelles tendances et à vous amuser avec votre garde-robe.",
+        '2023-06-13', 1),
 
-(3, 'Conseils pour un mode de vie sain', 'Découvrez des conseils simples pour maintenir un mode de vie sain et équilibré.', "Adoptez une alimentation équilibrée: Priviégiez une alimentation riche en fruits, légumes, grains entiers, protéines maigres et graisses saines. Évitez les aliments transformés et les excès de sucre, de sel et de matières grasses.
+       (3, 'Conseils pour un mode de vie sain',
+        'Découvrez des conseils simples pour maintenir un mode de vie sain et équilibré.', "Adoptez une alimentation équilibrée: Priviégiez une alimentation riche en fruits, légumes, grains entiers, protéines maigres et graisses saines. Évitez les aliments transformés et les excès de sucre, de sel et de matières grasses.
 
 Restez hydraté: Buvez suffisamment d'eau tout au long de la journée pour maintenir une bonne hydratation. Limitez votre consommation de boissons sucrées et d'alcool.
 
@@ -36,21 +40,37 @@ Dormez suffisamment: Accordez une importance à votre sommeil en visant environ 
 
 Limitez votre exposition aux substances nocives: Réduisez votre consommation de tabac, d'alcool et d'autres substances nocives pour votre santé. Évitez également l'exposition à la fumée secondaire et aux polluants environnementaux.
 
-Prenez des pauses régulières: Si vous travaillez sur un ordinateur ou passez beaucoup de temps assis, faites des pauses fréquentes pour vous étirer, bouger et reposer vos yeux.", '2023-06-12'),
-(4, 'Les meilleures destinations de voyage', 'Explorez les destinations de voyage les plus populaires et découvrez ce qu\'elles ont à offrir.', 'Le contenu de l\'article sur les meilleures destinations de voyage...', '2023-06-11'),
-(5, 'Les nouvelles avancées technologiques', 'Découvrez les dernières avancées technologiques qui façonnent notre avenir.', 'Le contenu de l\'article sur les nouvelles avancées technologiques...', '2023-06-10'),
-(6, 'Recettes faciles pour les amateurs de cuisine', 'Découvrez des recettes simples et délicieuses pour les passionnés de cuisine.', 'Le contenu de l\'article sur les recettes faciles pour les amateurs de cuisine...', '2023-06-09'),
-(7, 'Les films les plus attendus de l\'année', 'Découvrez les films les plus attendus qui sortiront prochainement au cinéma.', 'Le contenu de l\'article sur les films les plus attendus de l\'année...', '2023-06-08'),
-(8, 'Les bienfaits de l\'exercice physique', 'Apprenez les nombreux avantages d\'une activité physique régulière pour la santé et le bien-être.', 'Le contenu de l\'article sur les bienfaits de l\'exercice physique...', '2023-06-07'),
-(9, 'Les dernières actualités économiques', 'Restez informé des derniers développements économiques et financiers à travers le monde.', 'Le contenu de l\'article sur les dernières actualités économiques...', '2023-06-06'),
-(10, 'Les tendances en matière de décoration intérieure', 'Découvrez les dernières tendances en matière de décoration pour créer un intérieur moderne et élégant.', 'Le contenu de l\'article sur les tendances en matière de décoration intérieure...', '2023-06-05');
+Prenez des pauses régulières: Si vous travaillez sur un ordinateur ou passez beaucoup de temps assis, faites des pauses fréquentes pour vous étirer, bouger et reposer vos yeux.",
+        '2023-06-12', 2),
+       (4, 'Les meilleures destinations de voyage',
+        'Explorez les destinations de voyage les plus populaires et découvrez ce qu\'elles ont à offrir.',
+        'Le contenu de l\'article sur les meilleures destinations de voyage...', '2023-06-11', 2),
+       (5, 'Les nouvelles avancées technologiques',
+        'Découvrez les dernières avancées technologiques qui façonnent notre avenir.',
+        'Le contenu de l\'article sur les nouvelles avancées technologiques...', '2023-06-10', 3),
+       (6, 'Recettes faciles pour les amateurs de cuisine',
+        'Découvrez des recettes simples et délicieuses pour les passionnés de cuisine.',
+        'Le contenu de l\'article sur les recettes faciles pour les amateurs de cuisine...', '2023-06-09', 3),
+       (7, 'Les films les plus attendus de l\'année',
+        'Découvrez les films les plus attendus qui sortiront prochainement au cinéma.',
+        'Le contenu de l\'article sur les films les plus attendus de l\'année...', '2023-06-08', 4),
+       (8, 'Les bienfaits de l\'exercice physique',
+        'Apprenez les nombreux avantages d\'une activité physique régulière pour la santé et le bien-être.',
+        'Le contenu de l\'article sur les bienfaits de l\'exercice physique...', '2023-06-07', 4),
+       (9, 'Les dernières actualités économiques',
+        'Restez informé des derniers développements économiques et financiers à travers le monde.',
+        'Le contenu de l\'article sur les dernières actualités économiques...', '2023-06-06', 3),
+       (10, 'Les tendances en matière de décoration intérieure',
+        'Découvrez les dernières tendances en matière de décoration pour créer un intérieur moderne et élégant.',
+        'Le contenu de l\'article sur les tendances en matière de décoration intérieure...', '2023-06-05', 1);
 
-INSERT INTO `CATEGORIE` (`id`, `titre`, `description`) VALUES
-(1, 'Actualité', "Informations complètes et à jour, compilées à partir de sources d'actualités du monde entier."),
-(2, 'Evenements', 'Informations sur les evenements dans le grand est.'),
-(3, 'Prévention', 'Attention, notre prévention vous vient en aide'),
-(4, 'Cuisine', 'Découvrez des recettes délicieuses et des conseils culinaires.'),
-(5, 'Santé et bien-être', 'Informations et conseils pour maintenir une bonne santé physique et mentale.'),
-(6, 'Voyages', 'Inspiration et conseils pour vos prochaines aventures.'),
-(7, 'Technologie', 'Actualités et nouveautés technologiques.'),
-(8, 'Sport et fitness', 'Conseils, entraînements et actualités sportives pour rester en forme.');
+INSERT INTO `CATEGORIE` (`id`, `titre`, `description`)
+VALUES (1, 'Actualité',
+        "Informations complètes et à jour, compilées à partir de sources d'actualités du monde entier."),
+       (2, 'Evenements', 'Informations sur les evenements dans le grand est.'),
+       (3, 'Prévention', 'Attention, notre prévention vous vient en aide'),
+       (4, 'Cuisine', 'Découvrez des recettes délicieuses et des conseils culinaires.'),
+       (5, 'Santé et bien-être', 'Informations et conseils pour maintenir une bonne santé physique et mentale.'),
+       (6, 'Voyages', 'Inspiration et conseils pour vos prochaines aventures.'),
+       (7, 'Technologie', 'Actualités et nouveautés technologiques.'),
+       (8, 'Sport et fitness', 'Conseils, entraînements et actualités sportives pour rester en forme.');
