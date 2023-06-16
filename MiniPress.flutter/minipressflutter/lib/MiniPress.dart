0baffe266dd4c3e09bef079a97f1477d506419df
clemgrom <clement.gromangin5@etu.univ-lorraine.fr>
2023-06-16 11:28:42 +0200

    reboot categories articles utilisent api marche pas encore

--- MiniPress.flutter/minipressflutter/lib/MiniPress.dart
+++ MiniPress.flutter/minipressflutter/lib/MiniPress.dart
@@ -1,68 +1,26 @@
 import 'package:flutter/material.dart';
-import '../screens/CategoriesPage.dart';
-import '../screens/HomePage.dart';
-import '../screens/ComptePage.dart';
+import 'package:minipressflutter/screens/HomePage.dart';
 
 class MiniPress extends StatefulWidget {
-  const MiniPress({Key? key}) : super(key: key);
+  const MiniPress({ Key? key }) : super(key: key);
 
   @override
   _MiniPressState createState() => _MiniPressState();
 }
 
 class _MiniPressState extends State<MiniPress> {
-  int _currentIndex = 0; // Indice de l'onglet actuellement sélectionné
-
-  final List<Widget> _tabs = [
-    // Contenu des différents onglets
-    const HomePage(),
-    // const CategoriesPage(),
-    const ComptePage(),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MiniPress',
+      title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('MiniPress'),
+          title: const Center(child: Text('MiniPress')),
         ),
-        body:
-            _tabs[_currentIndex], // Affiche le contenu de l'onglet sélectionné
-
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (int index) {
-            // Met à jour l'indice de l'onglet sélectionné
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.category),
-              label: 'Categories',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.article),
-              label: 'Articles',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Compte',
-            ),
-          ],
-        ),
+        
+        body: const HomePage(),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
-}
-
-void main() {
-  runApp(MiniPress());
 }
