2745624b00709a1a17b16103b29f5d837928c549
Nzosim <nzosim54@gmail.com>
2023-06-19 16:54:07 +0200

    Articles

--- MiniPress.flutter/minipressflutter/lib/models/Articles.dart
+++ MiniPress.flutter/minipressflutter/lib/models/Articles.dart
@@ -2,24 +2,13 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter/material.dart';
 
-Future<Articles> fetchArticles() async {
-  final response =
-      await http.get(Uri.parse('http://localhost:20003/api/articles'));
-
-  if (response.statusCode == 200) {
-    return Articles.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to load articles');
-  }
-}
-
 class Articles {
-  final int id;
+  final String id;
   final String titre;
   final String resume;
-  final Text contenu;
-  final DateTime date_crea;
-  final int categorie_id;
+  final String contenu;
+  final String date_crea;
+  final int categ_id;
   final int author_id;
 
   const Articles(
@@ -28,18 +17,46 @@ class Articles {
       required this.resume,
       required this.contenu,
       required this.date_crea,
-      required this.categorie_id,
+      required this.categ_id,
       required this.author_id});
 
   factory Articles.fromJson(Map<String, dynamic> json) {
     return Articles(
-      id: json['id'] as int,
+      id: json['id'] as String,
       titre: json['titre'] as String,
       resume: json['resume'] as String,
-      contenu: json['contenu'] as Text,
-      date_crea: json['date_crea'] as DateTime,
-      categorie_id: json['categorie_id'] as int,
-      author_id: json['author_id'] as int,
+      contenu: json['contenu'] as String,
+      date_crea: json['date'] as String,
+      categ_id: json['categorie'] as int,
+      author_id: json['auteur'] as int,
     );
+  }
+}
+
+Future<List<Articles>> fetchArticles() async {
+  final response =
+      await http.get(Uri.parse('http://localhost:20003/api/articles'));
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> jsonData = jsonDecode(response.body);
+    final List<dynamic> articlesData = jsonData['articles'];
+    List<dynamic> res = [];
+    print(jsonData['articles'].length);
+    for (int i = 0; i < jsonData['articles'].length; i++) {
+      final articleInfo = await http.get(Uri.parse(
+          'http://localhost:20003/api' + jsonData['articles'][i]['href']));
+      final Map<String, dynamic> data = jsonDecode(articleInfo.body);
+      if (data['auteur'] == null) {
+        data['auteur'] = 0;
+      }
+      res.add(data);
+    }
+    res.forEach((elem) {
+      print(elem['date']);
+    });
+    print(res.length);
+    return res.map((json) => Articles.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load Articles');
   }
 }
--- MiniPress.flutter/minipressflutter/lib/screens/ArticlesPage.dart
+++ MiniPress.flutter/minipressflutter/lib/screens/ArticlesPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minipressflutter/models/Articles.dart';
 
 class ArticlesPage extends StatefulWidget {
   const ArticlesPage({Key? key}) : super(key: key);
@@ -8,8 +9,55 @@ class ArticlesPage extends StatefulWidget {
 }
 
 class _ArticlesPageState extends State<ArticlesPage> {
+  late Future<List<Articles>> futureArticles;
+
+  @override
+  void initState() {
+    super.initState();
+    futureArticles = fetchArticles();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Articles Page');
+    return MaterialApp(
+      title: 'MiniPress',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('MiniPress'),
+        ),
+        body: Center(
+          child: FutureBuilder<List<Articles>>(
+            future: futureArticles,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    final article = snapshot.data![index];
+                    return ListTile(
+                      title: Text(
+                        article.titre + '\n' + article.resume,
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.pink,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
