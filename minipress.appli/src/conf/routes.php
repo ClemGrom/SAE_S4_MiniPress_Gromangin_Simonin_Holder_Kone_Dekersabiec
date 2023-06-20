47592b5e1c392f537df1f154dc500b2ad542b442
ismaël koné <kone41u@iutnc-503-05.iutnc.site.univ-lorraine.fr>
2023-06-20 15:46:17 +0200

    routes updated : home page route added

--- minipress.appli/src/conf/routes.php
+++ minipress.appli/src/conf/routes.php
@@ -6,6 +6,7 @@ use minipress\app\actions\ArticleCreateAction;
 use minipress\app\actions\ArticleCreateHandler;
 
 return function($app) {
+    $app->get('[/]',HomeAction::class)->setName('home');
     $app->get('/article/create', ArticleCreateAction::class)->setName('article_create');
     $app->post('/article/create', ArticleCreateHandler::class)->setName('article_create_handler');
 };
