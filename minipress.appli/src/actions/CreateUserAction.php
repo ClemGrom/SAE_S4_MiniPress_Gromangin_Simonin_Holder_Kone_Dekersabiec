2021f9121eeb7bd9df8ea3016fac15f290d4325b
dekersab3u <dekersab3u@etu.univ-lorraine.fr>
2023-06-21 10:13:34 +0200

    avancement createuser : password strength

--- minipress.appli/src/actions/CreateUserAction.php
+++ minipress.appli/src/actions/CreateUserAction.php
@@ -5,6 +5,7 @@ namespace minpress\appli\action;
 use minipress\appli\services\AuthorServices;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\InvalidArgumentException;
 use Slim\Views\Twig;
 use minipress\appli\services\AuthorService;
 
@@ -22,10 +23,33 @@ class CreateUserAction extends Action
             return Twig::fromRequest($request)->render($response, 'Connect.twig', [
                 'author' => $as->getAuthorID($args['id'])
             ]);
-
         }
+    }
+
+
+
+    public function checkPasswordStrength(string $pass): bool {
+
+        $digit = preg_match("#[\d]#", $pass); // au moins un digit
+        $special = preg_match("#[\W]#", $pass); // au moins un car. spécial
+        $lower = preg_match("#[a-z]#", $pass); // au moins une minuscule
+        $upper = preg_match("#[A-Z]#", $pass); // au moins une majuscule
+        if ( !$digit || !$special || !$lower || !$upper)return false;
+        return true;
 
     }
 
+    public function CreateUser(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        //recupere donnees post
+        if (strlen($args['password']) < 10) {
+            throw new InvalidArgumentException('Le mot de passe doit contenir au moins 10 caractères');
+        }
+
+        if (checkPasswordStrength($args['password']) == false) {
+            throw new InvalidArgumentException('Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial');
+        }
+
+    }
 
 }
