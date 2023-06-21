4a69f32d88e4333f07050c183d9a8e4ef2a80047
Jules <jules.holder.54@gmail.com>
2023-06-21 08:57:24 +0200

    appli namespace et params, mais marche pas encore

--- minipress.appli/src/actions/ArticleCreateAction.php
+++ minipress.appli/src/actions/ArticleCreateAction.php
@@ -1,16 +1,15 @@
 <?php
 
-namespace minipress\app\actions;
+namespace minipress\appli\actions;
 
+use minipress\app\actions\Action;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface;
-use minipress\app\models\Article;
-use minipress\app\models\Categorie;
 use Slim\Views\Twig;
 
 class ArticleCreateAction extends Action
 {
-    public function __invoke(ServerRequestInterface $request, Response $response, $args):Response
+    public function __invoke(ServerRequestInterface $request, Response $response, array $args): Response
     {
         return Twig::fromRequest($request)->render($response, 'ArticleCreateForm.twig');
     }
