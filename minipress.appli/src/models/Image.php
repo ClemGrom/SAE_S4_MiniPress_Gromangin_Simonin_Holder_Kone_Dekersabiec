5dfa28823ef1619c76fe8f2fddf2ed11b17893a9
Jules <jules.holder.54@gmail.com>
2023-06-21 19:15:48 +0200

    appli relations pour les images

--- minipress.appli/src/models/Image.php
+++ minipress.appli/src/models/Image.php
@@ -3,11 +3,22 @@
 namespace minipress\app\models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use minipress\api\models\Article;
 
 class Image extends Model
 {
     protected $table = 'image';
     protected $primaryKey = 'id';
     public $timestamps = false;
+    public $incrementing = true;
+    public $keyType = 'string';
+    public $fillable = ['id', 'url'];
+
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany(Article::class, 'image2article', 'image_id',
+            'article_id');
+    }
 
 }
