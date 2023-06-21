f14403e6fb72422405eaf2d386fd60646d7315a4
Jules <jules.holder.54@gmail.com>
2023-06-21 11:06:08 +0200

    appli changement nom

--- minipress.appli/src/actions/GetArticleCreateAction.php
+++ minipress.appli/src/actions/GetArticleCreateAction.php
@@ -7,7 +7,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Views\Twig;
 
-class ArticleCreateAction extends Action
+class GetArticleCreateAction extends Action
 {
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
