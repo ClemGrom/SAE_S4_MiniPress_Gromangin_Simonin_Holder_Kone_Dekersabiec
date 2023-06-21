31e319baf5646cb154aa19b4c834cb73d421b056
Jules <jules.holder.54@gmail.com>
2023-06-21 19:17:17 +0200

    appli addImage to ArticleService.php

--- minipress.appli/src/services/ArticleService.php
+++ minipress.appli/src/services/ArticleService.php
@@ -6,7 +6,7 @@ use minipress\app\models\Article;
 
 class ArticleService
 {
-    public static function createArticle(array $data): void
+    public static function createArticle(array $data): Article
     {
         $article = new Article();
         $article->titre = $data['titre'];
@@ -15,5 +15,14 @@ class ArticleService
         $article->date_crea = date('Y-m-d H:i:s');
         $article->categ_id = $data['categorie'];
         $article->save();
+        return $article;
+    }
+
+    public static function addImage(string $id, string $image): void
+    {
+        if ($image != null) {
+            $article = Article::findOrFail($id);
+            $article->images()->attach($image);
+        }
     }
 }
