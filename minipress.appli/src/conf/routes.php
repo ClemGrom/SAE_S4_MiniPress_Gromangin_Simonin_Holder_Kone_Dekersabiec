276975819a163076b91c5c6a68a92547b976adfb
Jules <jules.holder.54@gmail.com>
2023-06-21 08:57:33 +0200

    appli namespace

--- minipress.appli/src/conf/routes.php
+++ minipress.appli/src/conf/routes.php
@@ -2,8 +2,8 @@
 
 namespace minipress\app\conf;
 
-use minipress\app\actions\ArticleCreateAction;
-use minipress\app\actions\ArticleCreateHandler;
+use minipress\appli\actions\ArticleCreateAction;
+use minipress\appli\actions\ArticleCreateHandler;
 
 return function($app) {
     $app->get('/article/create', ArticleCreateAction::class)->setName('article_create');
