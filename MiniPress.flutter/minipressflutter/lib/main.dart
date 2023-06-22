c3d7caefd1eb98fce0aaf022d00302426572763d
Nzosim <nzosim54@gmail.com>
2023-06-22 09:39:33 +0200

    import

--- MiniPress.flutter/minipressflutter/lib/main.dart
+++ MiniPress.flutter/minipressflutter/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:minipressflutter/screens/MiniPress.dart';
-import 'models/categories.dart';
 
 void main() {
   //lancement de l'application
--- MiniPress.flutter/minipressflutter/test/widget_test.dart
+++ MiniPress.flutter/minipressflutter/test/widget_test.dart
@@ -7,8 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:minipressflutter/main.dart';
 import 'package:minipressflutter/screens/MiniPress.dart';
 
 void main() {
