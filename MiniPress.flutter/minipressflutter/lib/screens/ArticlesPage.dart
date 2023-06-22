cce2b4756e2b33f9a6fdb8067422d47712fe4e47
Nzosim <nzosim54@gmail.com>
2023-06-22 09:22:30 +0200

    unused import

--- MiniPress.flutter/minipressflutter/lib/screens/ArticlesPage.dart
+++ MiniPress.flutter/minipressflutter/lib/screens/ArticlesPage.dart
@@ -1,5 +1,3 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:minipressflutter/models/Articles.dart';
 import 'package:minipressflutter/screens/ArticleDetails.dart';
