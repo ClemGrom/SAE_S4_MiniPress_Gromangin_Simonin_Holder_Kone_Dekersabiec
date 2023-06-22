a36bec1f1781955c32e10250f013dd2e83c37fba
dekersab3u <dekersab3u@etu.univ-lorraine.fr>
2023-06-22 14:44:49 +0200

    mise à jour routes appli

--- minipress.appli/src/conf/routes.php
+++ minipress.appli/src/conf/routes.php
@@ -4,6 +4,7 @@ namespace minipress\app\conf;
 
 use minipress\app\actions\HomeAction;
 use minipress\app\actions\CreateUserAction;
+use minipress\app\actions\ConnectUserAction;
 use minipress\app\actions\GetAllArticlesAction;
 use minipress\app\actions\GetArticleCreateAction;
 use minipress\app\actions\GetCategorieCreateAction;
@@ -14,13 +15,15 @@ return function ($app) {
 
     //utilisateurs
     $app->get('/inscription', CreateUserAction::class)->setName('inscription_users');
+    $app->get('/connection', ConnectUserAction::class)->setName('connection_users');
 
     //page d'acceuil
     $app->get('[/]', HomeAction::class)->setName('home_page');
 
     //liste des articles
     $app->get('/articles', GetAllArticlesAction::class)->setName('list_articles');
-    
+    $app->get('/articles/{categorie}', GetAllArticlesAction::class)->setName('list_articles');
+
     //Création d'article
     $app->get('/article/create', GetArticleCreateAction::class)->setName('article_create');
     $app->post('/article/create', PostArticleCreateAction::class);
